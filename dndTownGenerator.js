de3186eceb65c999e786ed5d08e1f555431a662d
huntergoodman <hunterleegoodman@outlook.com>
2019-05-16 10:14:03 -0500

    Add files via upload

---
+++ dndTownGenerator.js
@@ -0,0 +1,11 @@
+//town arrays
+var myTowns = ['Rowark', 'Uldark', 'Mularj', 'Mazk', 'Volki', 'Shai'];
+var myDescriptions = ['The town looks bustling, as though they are getting ready for a festival. The towns economy appears to be in good standing. All the businesses seem to be profitable. The blacksmith especially. However, the town hides a dark secret...', 'The town is old; rundown. All the windows on the houses are boarded up and so are the doors. There is a small tavern that remains open and a old looking inn. The towns economy from what it looks like virtually does not exist.', 'The city looks to be kept clean and free of pests in the streets. The scent of wild animals and sweat can be smelled throughout the city. The townsfolk are wary of strangers and seem untalkative.'];
+
+//random town
+var town = myTowns[Math.floor(Math.random() * myTowns.length)];
+var description = myDescriptions[Math.floor(Math.random() * myDescriptions.length)];
+
+
+
+
